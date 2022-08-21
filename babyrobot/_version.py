7a412ce37060d168b3cddaafda124597dc1fb553
LisburnLad <TinkertyTonk@hotmail.co.uk>
2022-08-21 11:36:37 +0100

    Added Policy and Value Iteration notebook

--- babyrobot/_version.py
+++ babyrobot/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Steve Roberts
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (1,0,7)
+version_info = (1,0,8)
 __version__ = ".".join(map(str, version_info))
