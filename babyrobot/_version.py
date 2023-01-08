0ddb4575859ed6251ae714031f2e9b4ac4f68941
LisburnLad <TinkertyTonk@hotmail.co.uk>
2023-01-08 12:51:48 +0000

    BabyRobot Version 1.0.22

--- babyrobot/_version.py
+++ babyrobot/_version.py
@@ -1,5 +1,5 @@
 # Copyright (c) Steve Roberts
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (1,0,20)
+version_info = (1,0,22)
 __version__ = ".".join(map(str, version_info))
